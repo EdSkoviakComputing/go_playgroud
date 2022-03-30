a445654b5e5a5097c82b7deeedf236c90b1f6540
eskoviak <eskoviak@eskoviak.com>
2022-03-30 10:23:27 -0500

    updated and created develop

--- learnpackage/main.go
+++ learnpackage/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"learnpackage/simpleinterest"
+	"math/rand"
 )
 
 func main() {
+	rand.Seed(86)
 	fmt.Println("Simple interest calculation")
-	p := 5000.0
+	p := rand.Float64()*5000.0
+	fmt.Println("Principal: ", p)
 	r := 10.0
 	t := 1.0
 	si := simpleinterest.Calculate(p, r, t)
